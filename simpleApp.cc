bd2d32ba95b108b8494db145b4c08f05af9d9316
Tintamar <martin.hermant@gmail.com>
2020-01-27 13:14:06 +0100

    simpleApp

---
+++ simpleApp.cc
@@ -0,0 +1,25 @@
+//bin/echo; [ $(uname) = "Darwin" ] && FLAGS="-framework Webkit" || FLAGS="$(pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0)" ; c++ "$0" $FLAGS -std=c++11 -g -o webview && ./webview ; exit
+// +build ignore
+
+#include "webview.h"
+
+#include <iostream>
+
+#ifdef _WIN32
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                     LPSTR lpCmdLine, int nCmdShow)
+#else
+int main()
+#endif
+{
+  webview::webview w(true, nullptr);
+  w.set_title("App");
+  w.set_size(1080, 720, WEBVIEW_HINT_NONE);
+  w.set_size(300, 200, WEBVIEW_HINT_MIN);
+  
+
+  w.navigate("http://localhost:8081");
+ 
+  w.run();
+  return 0;
+}
