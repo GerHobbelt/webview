ab9f3eb6ba7103a2ff74399fcc1ff69ce2e11ca6
Tintamar <martin.hermant@gmail.com>
2020-01-28 16:18:37 +0100

    XX

--- main.cc
+++ main.cc
@@ -13,35 +13,10 @@ int main()
 #endif
 {
   webview::webview w(true, nullptr);
-  w.set_title("Example");
-  w.set_size(480, 320, WEBVIEW_HINT_NONE);
+  w.set_title("App");
+  w.set_size(1080, 800, WEBVIEW_HINT_NONE);
   w.set_size(180, 120, WEBVIEW_HINT_MIN);
-  w.bind("noop", [](std::string s) -> std::string {
-    std::cout << s << std::endl;
-    return s;
-  });
-  w.bind("add", [](std::string s) -> std::string {
-    auto a = std::stoi(webview::json_parse(s, "", 0));
-    auto b = std::stoi(webview::json_parse(s, "", 1));
-    return std::to_string(a + b);
-  });
-  w.navigate(R"(data:text/html,
-    <!doctype html>
-    <html>
-      <body>hello</body>
-      <script>
-        window.onload = function() {
-          document.body.innerText = `hello, ${navigator.userAgent}`;
-          noop('hello').then(function(res) {
-            console.log('noop res', res);
-          });
-          add(1, 2).then(function(res) {
-            console.log('add res', res);
-          });
-        };
-      </script>
-    </html>
-  )");
+  w.navigate(R"http://localhost:8081");
   w.run();
   return 0;
 }
